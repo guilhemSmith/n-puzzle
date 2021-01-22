7e150aaa2ad5ad0e319f3a7a09fd95fc39a754b9
Guilhem SMITH <gsmith@student.42.fr>
2021-01-22 11:22:15 +0100

    Add g and h values to state definition.

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -4,6 +4,8 @@ use std::fmt;
 pub struct State {
 	cells: Vec<u8>,
 	size: usize,
+	cost_g: i32,
+	dist_h: i32,
 }
 
 impl State {
@@ -11,19 +13,40 @@ impl State {
 		if cells.len() != size * size {
 			panic!("Trying to create incorrectly sized state.")
 		}
-		State { cells, size }
+		State {
+			cells,
+			size,
+			cost_g: 0,
+			dist_h: 0,
+		}
 	}
 
 	fn access(&self, x: usize, y: usize) -> u8 {
 		self.cells[x + y * self.size]
 	}
+
+	pub fn cost_g(&self) -> &i32 {
+		&self.cost_g
+	}
+
+	pub fn cost_g_mut(&mut self) -> &mut i32 {
+		&mut self.cost_g
+	}
+
+	pub fn dist_h(&self) -> &i32 {
+		&self.dist_h
+	}
+
+	pub fn dist_h_mut(&mut self) -> &mut i32 {
+		&mut self.dist_h
+	}
 }
 
 impl fmt::Display for State {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		let mut output = String::from("");
+		let mut output = format!("  cost_g: {}\n  dist_h: {}\n", self.cost_g, self.dist_h);
 		for i in 0..self.size {
-			output.push('|');
+			output.push_str("\n|");
 			for j in 0..self.size {
 				output += &format!(
 					" {:^size$} ",
@@ -32,9 +55,6 @@ impl fmt::Display for State {
 				)
 			}
 			output.push('|');
-			if i < self.size - 1 {
-				output.push('\n');
-			}
 		}
 		write!(f, "{}", output)
 	}
