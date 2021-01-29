84cfed9d3307aa84143c903776648c5c5afef6f8
Guilhem SMITH <gsmith@student.42.fr>
2021-01-29 15:22:57 +0100

    Remove duplicate data.

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -11,27 +11,31 @@ pub fn a_star(
 ) -> puzzle::Solution {
 	let mut closed_set: HashSet<puzzle::StateUsed> = HashSet::new();
 	let mut open_queue: BinaryHeap<Reverse<puzzle::StateUnknown>> = BinaryHeap::new();
-	let mut opened: HashSet<Vec<u8>> = HashSet::new();
 	let mut solution = puzzle::Solution::new();
 
 	*(start.score_mut()) = heuristic(&start, &goal);
-	opened.insert(start.cells().clone());
 	open_queue.push(Reverse(start));
 	while let Some(Reverse(current_state)) = open_queue.pop() {
-		let current_used: puzzle::StateUsed = (&current_state).into();
-		opened.remove(current_used.cells());
+		let current_used = (&current_state).into();
 		if current_state.cells() == goal.cells() {
 			return solution.build_solution(closed_set, current_used);
 		}
 		for mut neighbor in current_state.neighbors() {
 			*(neighbor.cost_mut()) = current_state.cost() + 1;
 			*(neighbor.score_mut()) = neighbor.cost() + heuristic(&neighbor, &goal);
-			if !(closed_set.contains(neighbor.cells()) || opened.contains(neighbor.cells())) {
-				if !opened.insert(neighbor.cells().clone()) {
-					let mut tmp = open_queue.into_sorted_vec();
-					if let Some(index) = tmp.iter().position(|Reverse(state)| {
-						state.cells() == neighbor.cells() && state.cost() <= neighbor.cost()
-					}) {
+			if !(closed_set.contains(neighbor.cells())
+				|| open_queue.iter().any(|Reverse(state)| {
+					state.cells() == neighbor.cells() && state.cost() <= neighbor.cost()
+				})) {
+				if open_queue
+					.iter()
+					.any(|Reverse(state)| state.cells() == neighbor.cells())
+				{
+					let mut tmp = open_queue.into_vec();
+					if let Some(index) = tmp
+						.iter()
+						.position(|Reverse(state)| state.cells() == neighbor.cells())
+					{
 						tmp.remove(index);
 					}
 					open_queue = BinaryHeap::from(tmp);
