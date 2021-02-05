9fb8ecc4f18e94e27ef59396915d3ecd49c52178
Guilhem SMITH <gsmith@student.42.fr>
2021-02-05 11:33:17 +0100

    linear conflict heuristic.

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -10,9 +10,9 @@ pub fn manhattan(a: &puzzle::StateUnknown, b: &puzzle::StateUnknown) -> i32 {
 			let value = a.access(x, y);
 			if value != 0 {
 				let (x_target, y_target) = b.coord(value);
-				let dx = x_target - x;
-				let dy = y_target - y;
-				manhattan += dx.abs() + dy.abs();
+				let dx = (x_target - x).abs();
+				let dy = (y_target - y).abs();
+				manhattan += dx + dy;
 			}
 		}
 	}
@@ -34,4 +34,67 @@ pub fn hamming(a: &puzzle::StateUnknown, b: &puzzle::StateUnknown) -> i32 {
 		}
 	}
 	return hamming;
+}
+
+pub fn linear_conflict_manhattan(a: &puzzle::StateUnknown, b: &puzzle::StateUnknown) -> i32 {
+	let mut manhattan = 0;
+	let mut linear_confict = 0;
+	let n = a.size() as i32;
+	let d = (n * n) as usize;
+	let mut aligned: Vec<(bool, bool)> = Vec::with_capacity(d);
+	aligned.resize(d, (false, false));
+	for x in 0..n {
+		for y in 0..n {
+			let value = a.access(x, y);
+			if value != 0 {
+				let (x_target, y_target) = b.coord(value);
+				let dx = (x_target - x).abs();
+				let dy = (y_target - y).abs();
+				manhattan += dx + dy;
+				aligned[(x + y * n) as usize] = (dx == 0, dy == 0);
+				if dx == 0 {
+					for r in y..n {
+						a.access(x, r);
+					}
+				}
+				if dy == 0 {}
+			}
+		}
+	}
+	for x in 0..n {
+		for y in 0..n {
+			let value = a.access(x, y);
+			if value != 0 {
+				let (x_target, y_target) = b.coord(value);
+				let (align_x, align_y) = aligned[(x + y * n) as usize];
+				if align_x {
+					for r in y..n {
+						let other = a.access(x, r);
+						if other != 0 {
+							if let (true, _) = aligned[(x + r * n) as usize] {
+								let (_, y_other_target) = b.coord(other);
+								if y_other_target < y_target {
+									linear_confict += 1;
+								}
+							}
+						}
+					}
+				}
+				if align_y {
+					for c in x..n {
+						let other = a.access(c, y);
+						if other != 0 {
+							if let (_, true) = aligned[(c + y * n) as usize] {
+								let (x_other_target, _) = b.coord(other);
+								if x_other_target < x_target {
+									linear_confict += 1;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return manhattan + 2 * linear_confict;
 }
