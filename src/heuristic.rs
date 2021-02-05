537a7da5f00b64a4dd6cc0a9d6a555f2b3cbaefd
Guilhem SMITH <gsmith@student.42.fr>
2021-02-05 09:54:00 +0100

    Hamming distance heuristic.

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -18,3 +18,20 @@ pub fn manhattan(a: &puzzle::StateUnknown, b: &puzzle::StateUnknown) -> i32 {
 	}
 	return manhattan;
 }
+
+pub fn hamming(a: &puzzle::StateUnknown, b: &puzzle::StateUnknown) -> i32 {
+	let mut hamming = 0;
+	let n = a.size() as i32;
+	for x in 0..n {
+		for y in 0..n {
+			let value = a.access(x, y);
+			if value != 0 {
+				let (x_target, y_target) = b.coord(value);
+				if x_target != x && y_target != y {
+					hamming += 1;
+				}
+			}
+		}
+	}
+	return hamming;
+}
