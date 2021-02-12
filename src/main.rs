2f5a797e3a319fd9c89df3bd370874da8f565b32
Guilhem SMITH <gsmith@student.42.fr>
2021-02-12 11:01:13 +0100

    split main from setup.

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,7 @@ use colored::*;
 use std::error;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let args = arguments::get();
-
-    let heuristic = algorithm::Heuristic::get(args.value_of("heuristic").unwrap()).unwrap();
-    let search_type = algorithm::SearchType::get(args.value_of("search_type").unwrap()).unwrap();
-    let (size, start) = if let Some(filename) = args.value_of("file") {
-        let (size, start) = generation::from_file(filename)?;
-        println!("puzzle parsed:\n{}", start);
-        (size, start)
-    } else {
-        let size = args.value_of("size").unwrap_or("3").parse()?;
-        let without_solution = args.is_present("without_solution");
-        let iterations = args.value_of("iterations").unwrap_or("1000").parse()?;
-        let start = generation::random(size, !without_solution, iterations);
-        println!("puzzle generated:\n{}", start);
-        (size, start)
-    };
+    let (size, start, heuristic, search_type) = setup()?;
     let goal = puzzle::StateUnknown::goal(size);
     let split_line = format!(
         " {:-^size$} ",
@@ -52,4 +37,31 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         solution.size_complexity()
     );
     Ok(())
+}
+fn setup() -> Result<
+    (
+        usize,
+        puzzle::StateUnknown,
+        algorithm::Heuristic,
+        algorithm::SearchType,
+    ),
+    Box<dyn error::Error>,
+> {
+    let args = arguments::get();
+
+    let heuristic = algorithm::Heuristic::get(args.value_of("heuristic").unwrap()).unwrap();
+    let search_type = algorithm::SearchType::get(args.value_of("search_type").unwrap()).unwrap();
+    let (size, start) = if let Some(filename) = args.value_of("file") {
+        let (size, start) = generation::from_file(filename)?;
+        println!("puzzle parsed:\n{}", start);
+        (size, start)
+    } else {
+        let size = args.value_of("size").unwrap_or("3").parse()?;
+        let without_solution = args.is_present("without_solution");
+        let iterations = args.value_of("iterations").unwrap_or("1000").parse()?;
+        let start = generation::random(size, !without_solution, iterations);
+        println!("puzzle generated:\n{}", start);
+        (size, start)
+    };
+    return Ok((size, start, heuristic, search_type));
 }
